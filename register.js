5a13411142c979c5bf6549bc111a2305704d4af5
ameenamahi <124766960+ameenamahi@users.noreply.github.com>
2024-06-14 14:39:21 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCLu3Y0Zy-ryamlO59znI-V9RgIbGv-zTE",
+    authDomain: "login-example-73688.firebaseapp.com",
+    projectId: "login-example-73688",
+    storageBucket: "login-example-73688.appspot.com",
+    messagingSenderId: "39199891951",
+    appId: "1:39199891951:web:072c2636b28769ed4c161a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
